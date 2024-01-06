d9982a813528c5e214f2fee7e428fac2ed0002a0
F33RNI <xxoinvizionxx@gmail.com>
2024-01-06 01:13:17 -0500

    style: update copyright date

--- Style.js
+++ Style.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 Fern Lane, Destiny's cards app
+ * Copyright (C) 2023-2024 Fern Lane, Destiny's cards app
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
